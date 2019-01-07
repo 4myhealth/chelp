1adaf4e5639c27ce15a9d0e8b3e336618475924b
Mattias Horvath <mh@4myhealth.at>
2019-01-07 08:53:11 +0100

    working progress

--- vue.config.js
+++ vue.config.js
@@ -7,13 +7,18 @@ module.exports = {
     },
     electronBuilder: {
       builderOptions: {
+        win: {
+          icon: './icon.ico',
+        },
+        mac: {
+          icon: './icon.png',
+        },
         // options placed here will be merged with default configuration and passed to electron-builder
         publish: [{
           provider: 'github',
           owner: '4myhealth',
           repo: 'chelp',
         }],
-
       },
     },
   },
